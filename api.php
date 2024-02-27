965b14723676f2bb2ceb98d01e20196b071a9704
T1W3R <werquin.timothe@gmail.com>
2024-02-27 14:57:22 +0100

    bdd/api a jour

--- api.php
+++ api.php
@@ -6,7 +6,7 @@ $url = $_SERVER['REQUEST_URI'];
 $chemin = ltrim(parse_url($url, PHP_URL_PATH));
 
 $chemin = substr($chemin, 30);
-echo($chemin);
+//echo $chemin;
 
 $database = "bdd.db";
 
@@ -27,111 +27,34 @@ if ($chemin == "") {
     $mdp = $_POST['mdp'];
     $statut = $_POST['statut'];
 
-    // Préparation de la requête d'insertion
-    $query = "INSERT INTO Utilisateur (ut_prenom, ut_nom, ut_email, ut_telephone, ut_mdp, ut_statut) VALUES (:prenom, :nom, :email, :telephone, :mdp, :statut)";
-    $stmt = $conn->prepare($query);
-    $stmt->bindValue(':prenom', $prenom, SQLITE3_TEXT);
-    $stmt->bindValue(':nom', $nom, SQLITE3_TEXT);
-    $stmt->bindValue(':email', $email, SQLITE3_TEXT);
-    $stmt->bindValue(':telephone', $telephone, SQLITE3_TEXT);
-    $stmt->bindValue(':mdp', $mdp, SQLITE3_TEXT);
-    $stmt->bindValue(':statut', $statut, SQLITE3_TEXT);
-    $stmt->execute();
-
-    // Récupération de l'ID de l'utilisateur inséré
-    $select = "SELECT ut_id FROM Utilisateur WHERE ut_nom = :nom AND ut_prenom = :prenom AND ut_mdp = :mdp LIMIT 1";
-    $stmt = $conn->prepare($select);
-    $stmt->bindValue(':nom', $nom, SQLITE3_TEXT);
-    $stmt->bindValue(':prenom', $prenom, SQLITE3_TEXT);
-    $stmt->bindValue(':mdp', $mdp, SQLITE3_TEXT);
-    $result = $stmt->execute();
-
-    if ($result) {
-        $row = $result->fetchArray(SQLITE3_ASSOC);
-        echo json_encode($row);
-    } else {
-        echo json_encode(array('ut_id' => 'refused'));
-    }
+    inscription($prenom, $nom, $email, $telephone, $mdp, $statut, $conn);
+    connexion($email, $mdp, $conn);
 
 } else if ($chemin == "connexion") {
     $email = $_POST['email'];
     $mdp = $_POST['mdp'];
 
-    var_dump($mdp, $email);
-
-    $query = "SELECT ut_id FROM Utilisateur WHERE ut_email = '$email' AND ut_mdp = '$mdp'";
-    //$query = "SELECT name FROM sqlite_master WHERE type='table'";
-    $result = $conn->query($query);
-
-    if ($result) {
-        $row = $result->fetchArray(SQLITE3_ASSOC);
-        if ($row) {
-            echo json_encode($row);
-        } else {
-            echo json_encode(array('ut_id' => 'refused'));
-        }
-    } else {
-        echo json_encode(array('ut_id' => 'refused'));
-    }
+    connexion($email, $mdp, $conn);
+    
 } else if ($chemin == "getUncompletedGarde") {
-    
-    $query = "SELECT ga_id, ga_adresse, ga_description, fk_utilisateur_proprietaire AS proprio, fk_utilisateur_volontaire AS volontaire FROM Garde WHERE volontaire = ''";
-    $result = $conn->query($query);
+    gardesLibre($conn);
 
-    if ($result) {
-        $rows = array();
-        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            $rows[] = $row;
-        }
-        echo json_encode($rows);
-    } else {
-        echo json_encode(array('return' => 'error'));
-    }
-} else if ($chemin == "getUserInfo") {
+} else if ($chemin == "getUserInfos") {
     $id = $_POST['id'];
     
-    $query = "SELECT ut_nom, ut_prenom, ut_statut, ut_email, ut_telephone FROM Utilisateur WHERE ut_id = ". $id;
-    $result = $conn->query($query);
+    infosUtilisateur($id, $conn);
 
-    if ($result) {
-        $rows = array();
-        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            $rows[] = $row;
-        }
-        echo json_encode($rows);
-    } else {
-        echo json_encode(array('return' => 'error'));
-    }
 } else if ($chemin == "getMyGarde") {
-
     $id = $_POST['id'];
     
-    $query = "SELECT ga_id, ga_adresse, ga_description, fk_utilisateur_proprietaire AS proprio, fk_utilisateur_volontaire AS volontaire FROM Garde WHERE volontaire = ". $id ." OR proprio = ". $id;
-    $result = $conn->query($query);
-
-    if ($result) {
-        $rows = array();
-        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
-            $rows[] = $row;
-        }
-        echo json_encode($rows);
-    } else {
-        echo json_encode(array('return' => 'error'));
-    }
+    mesGardes($id, $conn);
     
 } else if ($chemin == "createGarde") {
-
-    echo ('test');
     $adresse = $_POST['adresse'];
     $description = $_POST['description'];
     $proprio = $_POST['proprio'];
     
-    $query = "INSERT INTO Garde (ga_adresse, ga_description, fk_utilisateur_proprietaire, fk_utilisateur_volontaire) VALUES (:adresse, :description, :proprio, '')";
-    $stmt = $conn->prepare($query);
-    $stmt->bindValue(':adresse', $adresse, SQLITE3_TEXT);
-    $stmt->bindValue(':description', $description, SQLITE3_TEXT);
-    $stmt->bindValue(':proprio', $proprio, SQLITE3_TEXT);
-    $stmt->execute();
+    creerGarde($adresse, $description, $proprio, $conn);
 
 } else if ($chemin == "createImage") {
     $path = $_POST['path'];
@@ -160,3 +83,101 @@ if ($chemin == "") {
     $stmt->execute();
 
 } 
+
+//Fonctions SQL
+function connexion($email, $mdp, $conn){
+    $query = "SELECT ut_id FROM Utilisateur WHERE ut_email = '$email' AND ut_mdp = '$mdp'";
+    $result = $conn->query($query);
+
+    if ($result) {
+        $row = $result->fetchArray(SQLITE3_ASSOC);
+        if ($row) {
+            echo json_encode($row);
+        } else {
+            echo json_encode(array('ut_id' => 'refused'));
+        }
+    } else {
+        echo json_encode(array('ut_id' => 'refused'));
+    }
+}
+
+function inscription($prenom, $nom, $email, $telephone, $mdp, $statut, $conn){
+    $query = "INSERT INTO Utilisateur (ut_prenom, ut_nom, ut_email, ut_telephone, ut_mdp, ut_statut) VALUES (:prenom, :nom, :email, :telephone, :mdp, :statut)";
+    $stmt = $conn->prepare($query);
+    $stmt->bindValue(':prenom', $prenom, SQLITE3_TEXT);
+    $stmt->bindValue(':nom', $nom, SQLITE3_TEXT);
+    $stmt->bindValue(':email', $email, SQLITE3_TEXT);
+    $stmt->bindValue(':telephone', $telephone, SQLITE3_TEXT);
+    $stmt->bindValue(':mdp', $mdp, SQLITE3_TEXT);
+    $stmt->bindValue(':statut', $statut, SQLITE3_TEXT);
+    $stmt->execute();
+}
+
+function gardesLibre($conn){
+    $query = "SELECT ga_id, ga_adresse, ga_description, fk_utilisateur_proprietaire AS proprio, fk_utilisateur_volontaire AS volontaire FROM Garde WHERE volontaire = ''";
+    $result = $conn->query($query);
+
+    if ($result) {
+        $rows = array();
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $rows[] = $row;
+        }
+        echo json_encode($rows);
+    } else {
+        echo json_encode(array('return' => 'error'));
+    }
+}
+
+function infosUtilisateur($id, $conn){
+    $query = "SELECT ut_nom, ut_prenom, ut_statut, ut_email, ut_telephone FROM Utilisateur WHERE ut_id = ". $id;
+    $result = $conn->query($query);
+
+    if ($result) {
+        $rows = array();
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $rows[] = $row;
+        }
+        echo json_encode($rows);
+    } else {
+        echo json_encode(array('return' => 'error'));
+    }
+}
+
+function mesGardes($id, $conn){
+    $query = "SELECT ga_id, ga_adresse, ga_description, fk_utilisateur_proprietaire AS proprio, fk_utilisateur_volontaire AS volontaire FROM Garde WHERE volontaire = ". $id ." OR proprio = ". $id;
+    $result = $conn->query($query);
+
+    if ($result) {
+        $rows = array();
+        while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            $rows[] = $row;
+        }
+        echo json_encode($rows);
+    } else {
+        echo json_encode(array('return' => 'error'));
+    }
+}
+
+function creerGarde($adresse, $description, $proprio, $conn) {
+    $query = "INSERT INTO Garde (ga_adresse, ga_description, fk_utilisateur_proprietaire, fk_utilisateur_volontaire) VALUES (:adresse, :description, :proprio, '')";
+    $stmt = $conn->prepare($query);
+    $stmt->bindValue(':adresse', $adresse, SQLITE3_TEXT);
+    $stmt->bindValue(':description', $description, SQLITE3_TEXT);
+    $stmt->bindValue(':proprio', $proprio, SQLITE3_TEXT);
+    $stmt->execute();
+
+    $query = "SELECT ga_id FROM Garde WHERE ga_adresse = '$adresse' AND ga_description = '$description' AND fk_utilisateur_proprietaire = '$proprio' AND fk_utilisateur_volontaire = '' ORDER BY ga_id DESC";
+    $result = $conn->query($query);
+
+    if ($result) {
+        $row = $result->fetchArray(SQLITE3_ASSOC);
+        if ($row) {
+            echo json_encode($row);
+        } else {
+            echo json_encode(array('ga_id' => 'refused'));
+        }
+    } else {
+        echo json_encode(array('ga_id' => 'refused'));
+    }
+}
+
